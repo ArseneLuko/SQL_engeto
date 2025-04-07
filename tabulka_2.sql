d0edd5e7ccd36f7b4058916d3f0d97b8402cfef7
ArseneLuko <arsene.luko@gmail.com>
2025-04-07 21:41:30 +0200

    Finální upload pro: Tabulka 2

--- tabulka_2.sql
+++ tabulka_2.sql
@@ -1,21 +1,25 @@
--- Tabulky pojmenujte  t_{jmeno}_{prijmeni}_project_SQL_secondary_final (pro dodatečná data o dalších evropských státech).
+-- Vytvoření druhé tabulky. Tabulky pojmenujte  t_{jmeno}_{prijmeni}_project_SQL_secondary_final (pro dodatečná data o dalších evropských státech).
 -- t_Lukas_Karasek_project_SQL_secondary_final
+-- ...připravte i tabulku s HDP, GINI koeficientem a populací dalších evropských států ve stejném období, jako primární přehled pro ČR.
 
--- uloží tabulku pro všechny evropské státy s daty z tabulky economies, 
+-- Vytvoří tabulku pro všechny evropské státy s daty z tabulky economies (HDP, GINI a populace), 
 -- z tabulky countries napojené na economies se použije filtr pro Evropské státy
 -- další podmínka: stejném rozmezí let jako je v první tabulce vytvořené pro tento projekt
 
 CREATE OR REPLACE TABLE t_Lukas_Karasek_project_SQL_secondary_final
 	SELECT 
-		e.*
+		e.country,
+		e.year,
+		e.GDP,
+		e.population,
+		e.gini
 	FROM economies e
 	INNER JOIN countries c ON e.country = c.country
 	WHERE 1 
 		AND c.continent = 'Europe'
 		AND e.year BETWEEN 
-			(SELECT MIN(payroll_year) FROM t_Lukas_Karasek_project_SQL_primary_final) 
-			AND (SELECT MAX(payroll_year) FROM t_Lukas_Karasek_project_SQL_primary_final);
+			(SELECT MIN(year) FROM t_Lukas_Karasek_project_SQL_primary_final) 
+			AND (SELECT MAX(year) FROM t_Lukas_Karasek_project_SQL_primary_final);
 
 SELECT *
-FROM t_Lukas_Karasek_project_SQL_secondary_final
-WHERE country LIKE 'Cz%';
+FROM t_Lukas_Karasek_project_SQL_secondary_final;

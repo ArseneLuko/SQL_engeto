4243e3529abba3b00d6990364873d3d779be7bef
ArseneLuko <arsene.luko@gmail.com>
2025-04-07 22:18:31 +0200

    Finální upload pro: Otázka č.3

--- otazka_3.sql
+++ otazka_3.sql
@@ -1,19 +1,20 @@
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
--- Vytvořím si view, kde bude průměr meziročního růstu cen za každou kategorii ptravin
 
--- ZÁVĚR: Nejpomaleji zdražuje kategorie Cukr krystalový, její průměrné meziroční zdražení je -1,92 % (vlastně zlevňuje)
-	-- doplnění, když bereme v úvahu kategorie které opravdu zdražují, tak nejpomaleji zdražuje 
+-- ZÁVĚR: Nejpomaleji zdražuje kategorie Cukr krystalový, její průměrné meziroční zdražení je -1,92 % (tedy zlevňuje)
+	-- doplnění, když bereme v úvahu kategorie které opravdu zdražují, tak nejpomaleji zdražují Banány žluté - 0,81 %
+
+-- Vytvořím si view, kde bude průměr meziročního růstu cen za každou kategorii ptravin
 
 CREATE OR REPLACE VIEW v_avg_change_for_all_categories AS
 	WITH avg_prices_per_years AS (
 		SELECT 
 			tf.price_name,
-			tf.price_year,
+			tf.year,
 			tf.price_avg,
-			LAG(tf.price_avg) OVER (PARTITION BY tf.price_name ORDER BY tf.price_year) AS price_avg_previous_year
+			LAG(tf.price_avg) OVER (PARTITION BY tf.price_name ORDER BY tf.year) AS price_avg_previous_year
 		FROM t_Lukas_Karasek_project_SQL_primary_final tf
-		-- GROUP BY tf.price_name, tf.price_year
-		ORDER BY tf.price_name, tf.price_year
+		GROUP BY tf.price_name, tf.year
+		ORDER BY tf.price_name, tf.year
 	), 
 	percentage_change_per_years AS (
 		SELECT 
@@ -27,14 +28,14 @@ CREATE OR REPLACE VIEW v_avg_change_for_all_categories AS
 	FROM percentage_change_per_years pcpy
 	GROUP BY pcpy.price_name;
 
--- Vyberu z view řádek s nejnižší hodnotou (kde se rovná svému minimu)
+-- Vyberu z view řádek s nejnižší hodnotou zdražování (kde se rovná svému minimu)
 SELECT
 	*
 FROM v_avg_change_for_all_categories
 WHERE avg_percentage_change = (SELECT MIN(avg_percentage_change) FROM v_avg_change_for_all_categories vacfac);
 
 
--- podmínkou vyřadím záporné hodnoty (ty kategorie které průměrně zlevnily)
+-- Podmínkou vyřadím záporné hodnoty (ty kategorie které průměrně zlevnily) pro zobrazení kategorie, která zdaržovala nejméně, ale nezlevňovala
 SELECT 
 	*
 FROM v_avg_change_for_all_categories

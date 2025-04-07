db666942b25e6e33b241d09e40e582e88453fada
ArseneLuko <arsene.luko@gmail.com>
2025-04-07 22:13:23 +0200

    Finální upload pro: Otázka č.2

--- otazka_2.sql
+++ otazka_2.sql
@@ -1,20 +1,19 @@
--- Otázka č. 2
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
 -- ZÁVĚR: 
 	-- V roce 2006 je možné si za průměrnou mzdu ze všech odvětví koupit 1 287 kg chleba a 1 437 litrů mléka
 	-- V roce 2018 je možné si za průměrnou mzdu ze všech odvětví koupit 1 342 kg chleba a 1 641 litrů mléka
 
--- Vytvořím si view kde jsou údaje pouze za mléko a chleba v prvním (nejnižším) a posledním (nejvyšším) sledovaném roce a průměr mezd za všechna odvětví
+-- Vytvoří pohled kde jsou údaje pouze za mléko a chleba v prvním (nejnižším) a posledním (nejvyšším) sledovaném roce a průměr mezd za všechna odvětví
 
-CREATE OR REPLACE VIEW v_chleb_mleko_prvni_posledni_rok AS
+CREATE OR REPLACE VIEW v_lukas_karasek_kchleb_mleko_prvni_posledni_rok AS
 	WITH chleb_a_mleko AS (
 		SELECT 
 			tfinal.payroll_avg,
 			-- tfinal.payroll_brunch_name, -- pomocná linka pro přehled
 			tfinal.price_avg,
 			tfinal.price_name,
-			tfinal.price_year,
+			tfinal.year,
 			tfinal.price_value,
 			tfinal.price_unit 
 		FROM t_Lukas_Karasek_project_SQL_primary_final tfinal
@@ -26,16 +25,18 @@ CREATE OR REPLACE VIEW v_chleb_mleko_prvni_posledni_rok AS
 		AVG(payroll_avg) AS avg_payroll_all_brunches,
 		price_avg,
 		price_name,
-		price_year,
+		year,
 		price_value,
 		price_unit
 	FROM chleb_a_mleko
 	WHERE 1
-		AND (price_year = (SELECT MIN(price_year) FROM chleb_a_mleko)
-		OR price_year = (SELECT MAX(price_year) FROM chleb_a_mleko))
-	GROUP BY price_name, price_year;
+		AND (year = (SELECT MIN(year) FROM chleb_a_mleko)
+		OR year = (SELECT MAX(year) FROM chleb_a_mleko))
+	GROUP BY price_name, year;
+
+-- Dotaz pro získání možného počtu koupených chlebů nebo mléka za průměrnou mzdu v prvním a poseldním roce
 
 SELECT 
 	*,
-	FLOOR(vchm.avg_payroll_all_brunches / vchm.price_avg) AS possible_to_buy -- funkce FLOOR() odstraní desetiná místa (zaokrouhlení vždy dolů), protože můžeme koupit jen celé kg nebo litry
-FROM v_chleb_mleko_prvni_posledni_rok vchm;
+	FLOOR(vchm.avg_payroll_all_brunches / vchm.price_avg) AS possible_to_buy, -- funkce FLOOR() odstraní desetiná místa (zaokrouhlení vždy dolů), protože můžeme koupit jen celé kg nebo litry
+FROM v_lukas_karasek_kchleb_mleko_prvni_posledni_rok vchm;
